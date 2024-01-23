bbe4d32e3d5ac20c4b78304f8b04e62498691260
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-01-23 23:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+var inputs = document.getElementsByClassName("code");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("keyup", function(event) {
+    if (event.keyCode === 8) {
+      if (this.value.length === 0) {
+        var previous = this.previousElementSibling;
+        if (previous) {
+          previous.focus();
+        }
+      }
+    } else {
+      if (this.value.length === this.maxLength) {
+        var next = this.nextElementSibling;
+        if (next) {
+          next.focus();
+        }
+      }
+    }
+  });
+}
